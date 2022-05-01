65b31e1a6eb34cd63f95dc41f9869b8cc0e7522a
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2022-04-30 17:17:58 -0700

    Add product view link and axios

--- client-app-react/src/features/catalog/ProductDetails.tsx
+++ client-app-react/src/features/catalog/ProductDetails.tsx
@@ -1,7 +1,11 @@
 import { Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
 
-export default function ProductDetails(){
-    return <Typography variant='h2'>
-        Product Details
-    </Typography>
+export default function ProductDetails() {
+    const {id} = useParams<{id: string}>();
+    return (
+        <Typography variant='h2'>
+            Product Details
+        </Typography>
+    ) 
 }
