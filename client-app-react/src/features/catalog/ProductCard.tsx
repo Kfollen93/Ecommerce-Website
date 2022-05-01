87c7a83516de82afdf93443d2b18dbfe5448b524
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2022-04-30 18:58:27 -0700

    Update card price

--- client-app-react/src/features/catalog/ProductCard.tsx
+++ client-app-react/src/features/catalog/ProductCard.tsx
@@ -27,7 +27,7 @@ export default function ProductCard({ product }: Props) {
       />
       <CardContent>
         <Typography gutterBottom color='secondary' variant="h5">
-          ${product.price.toFixed(2)}
+          ${(product.price / 100).toFixed(2)}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           {product.brand} / {product.type}
