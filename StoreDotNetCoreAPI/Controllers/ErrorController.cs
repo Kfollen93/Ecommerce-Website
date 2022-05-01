4425193f1e8ec6a3b6ebe13ad3daa2855c4fd602
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2022-05-01 08:55:29 -0700

    Create error controller holding http error responses

---
+++ StoreDotNetCoreAPI/Controllers/ErrorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace StoreDotNetCoreAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ErrorController : ControllerBase
+    {
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound() => NotFound();
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest() => BadRequest(new ProblemDetails{Title = "This is a bad request"});
+
+        [HttpGet("unauthorized")]
+        public ActionResult GetUnauthorized() => Unauthorized();
+
+        [HttpGet("validation-error")]
+        public ActionResult GetValidationError()
+        {
+            ModelState.AddModelError("Problem 1", "First error.");
+            ModelState.AddModelError("Problem 2", "Second error.");
+            return ValidationProblem();
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError() => throw new System.Exception("Server error");
+
+    }
+}
