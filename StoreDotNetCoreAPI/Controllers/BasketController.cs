c2eae1b9e1a874971d737a8788d5845822d593e5
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2022-05-10 19:44:05 -0700

    Create Basket controller with GetBasket method

---
+++ StoreDotNetCoreAPI/Controllers/BasketController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreDotNetCoreAPI.Data;
+using StoreDotNetCoreAPI.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreDotNetCoreAPI.Controllers
+{
+    public class BasketController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public BasketController(StoreContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+            var basket = await _context.Baskets
+                .Include(i => i.Items)
+                .ThenInclude(p => p.Product)
+                .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+
+            if (basket == null) return NotFound();
+            return Ok(basket);
+        }
+    }
+}
