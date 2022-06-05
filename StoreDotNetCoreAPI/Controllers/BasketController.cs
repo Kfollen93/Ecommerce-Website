b4b3250dc5b8b7d9459d62667118e5bc560536e2
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2022-06-05 12:59:57 -0700

    Create method to map Basket to DTO

--- StoreDotNetCoreAPI/Controllers/BasketController.cs
+++ StoreDotNetCoreAPI/Controllers/BasketController.cs
@@ -17,33 +17,17 @@ namespace StoreDotNetCoreAPI.Controllers
         private readonly StoreContext _context;
         public BasketController(StoreContext context) => _context = context;
 
-        [HttpGet]
+        [HttpGet(Name = "GetBasket")]
         public async Task<ActionResult<BasketDTO>> GetBasket()
         {
             var basket = await RetrieveBasket();
 
             if (basket == null) return NotFound();
-            return new BasketDTO 
-            { 
-                Id = basket.Id, 
-                BuyerId = basket.BuyerId,
-                Items = basket.Items.Select(item => new BasketItemDTO
-                {
-                    ProductId = item.ProductId,
-                    Name = item.Product.Name,
-                    Price = item.Product.Price,
-                    PictureUrl = item.Product.PictureUrl,
-                    Type = item.Product.Type,
-                    Brand = item.Product.Brand,
-                    Quantity = item.Quantity
-                }).ToList()
-            };
+            return MapBasketToDTO(basket);
         }
 
-
-
         [HttpPost]
-        public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
+        public async Task<ActionResult<BasketDTO>> AddItemToBasket(int productId, int quantity)
         {
             var basket = await RetrieveBasket();
             if (basket == null) basket = CreateBasket();
@@ -53,12 +37,10 @@ namespace StoreDotNetCoreAPI.Controllers
             basket.AddItem(product, quantity);
 
             var results = await _context.SaveChangesAsync() > 0;
-            if (results) return StatusCode(201);
+            if (results) return CreatedAtRoute("GetBasket", MapBasketToDTO(basket));
 
             return BadRequest(new ProblemDetails { Title = "Problem saving item to basket."});
         }
-
-
 
         [HttpDelete]
         public async Task<ActionResult> RemoveItemFromBasket(int productId, int quantity)
@@ -89,6 +71,25 @@ namespace StoreDotNetCoreAPI.Controllers
             var basket = new Basket { BuyerId = buyerId };
             _context.Baskets.Add(basket);
             return basket;
+        }
+
+        private BasketDTO MapBasketToDTO(Basket basket)
+        {
+            return new BasketDTO
+            {
+                Id = basket.Id,
+                BuyerId = basket.BuyerId,
+                Items = basket.Items.Select(item => new BasketItemDTO
+                {
+                    ProductId = item.ProductId,
+                    Name = item.Product.Name,
+                    Price = item.Product.Price,
+                    PictureUrl = item.Product.PictureUrl,
+                    Type = item.Product.Type,
+                    Brand = item.Product.Brand,
+                    Quantity = item.Quantity
+                }).ToList()
+            };
         }
     }
 }
